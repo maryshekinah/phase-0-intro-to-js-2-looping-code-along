ee0989ab4cd6d52fa8660153d31bfb62c8b11070
shekinah <nyamburamary1212@gmail.com>
2023-09-21 12:54:03 +0300

    Complete looping code along 2

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solutions in this file
+function writeCards(names, event) {
+    const messages = [];
+    for (let i = 0; i < names.length; i++) {
+      const message = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+      messages.push(message);
+    }
+    return messages;
+  }
+  
+  const thankYouMessages = writeCards(["Guadalupe", "Ollie", "Aki"], "surprise");
+  console.log(thankYouMessages);
+  function countDown(startingNumber) {
+    for (let i = startingNumber; i >= 0; i--) {
+      console.log(i);
+    }
+  }
+  
+  // Example usage:
+  countDown(10); // This will log numbers from 10 down to 0
+  
